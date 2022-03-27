5d552a806a8cdba209734a24b15933136755a1e5
David <Davidmaxsands@gmail.com>
2022-03-26 18:19:51 -0600

    added effect to pricing

--- src/Components/Investment/Investment.js
+++ src/Components/Investment/Investment.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import investmentPhoto from '../../Data/Copy of Photography Pricing 120421-2 (1).png'
 import Footer from '../HomeComponents/Footer'
+import Pricing from './Pricing'
 
 function Investment() {
+const [isHidden, setIsHidden] = useState(true)
+
+
+  setTimeout(() => {
+    setIsHidden(false)
+  }, 500)
+
   return (
     <div id='investment-main'>
       <div className=" split2" >
@@ -12,35 +20,14 @@ function Investment() {
             <div className="investment" id="pricing-guide">
               <h1 id='ivestment-port'>PORTRAIT</h1>
               <h2 id='invest-pricing'>pricing guide</h2><br/>
-              <h3>CLASSIC</h3>
-              <p><i>Starting at $200</i></p><br/>
-              <ul>
-                <li>~ 1 hour session</li>
-                <li>~ 1 location</li>
-                <li>~ 1-2 outfits</li>
-                <li>~ 25+ edited images & online gallery</li>
-                <li>~ Turnaround time is 2 weeks</li>
-              </ul><br/>
-              <h3>DELUXE</h3>
-              <p><i>Starting at $375</i></p><br/>
-              <ul>
-                <li>~ 2 hour session</li>
-                <li>~ 2 location</li>
-                <li>~ 2 outfits</li>
-                <li>~ 50+ edited images & online gallery</li>
-                <li>~ Turnaround time is 4 weeks</li>
-              </ul><br/>
-              <h3>LUXURY</h3>
-              <p><i>Starting at $550</i></p><br/>
-              <ul>
-                <li>~ 3 hour session</li>
-                <li>~ 2-3 location</li>
-                <li>~ 2-3 outfits</li>
-                <li>~ 75+ edited images & online gallery</li>
-                <li>~ Turnaround time is 5 weeks</li>
-              </ul>
+              {isHidden ? null : <Pricing />}
+              
+              {/* <div id='email-container'>
+                <p id='email-add'>Don’t see what you’re looking for? Please fill out an inquiry with details about your project or event so we can create a custom package for you. Can’t wait to work with you and bring your vision to life! All other inquiries, please e-mail photography@anyssasands.com</p>
+              </div> */}
             </div>
         </div>
+        
         <Footer />
     </div>
   )
---
+++ src/Components/Investment/Pricing.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+function Pricing() {
+  return (
+    <div className='pricing-section'>
+        <h3>CLASSIC</h3>
+          <p><i>Starting at $200</i></p><br/>
+          <ul>
+            <li>~ 1 hour session</li>
+            <li>~ 1 location</li>
+            <li>~ 1-2 outfits</li>
+            <li>~ 25+ edited images & online gallery</li>
+            <li>~ Turnaround time is 2 weeks</li>
+          </ul><br/>
+          <h3>DELUXE</h3>
+          <p><i>Starting at $375</i></p><br/>
+          <ul>
+            <li>~ 2 hour session</li>
+            <li>~ 2 location</li>
+            <li>~ 2 outfits</li>
+            <li>~ 50+ edited images & online gallery</li>
+            <li>~ Turnaround time is 4 weeks</li>
+          </ul><br/>
+          <h3>LUXURY</h3>
+          <p><i>Starting at $550</i></p><br/>
+          <ul>
+            <li>~ 3 hour session</li>
+            <li>~ 2-3 location</li>
+            <li>~ 2-3 outfits</li>
+            <li>~ 75+ edited images & online gallery</li>
+            <li>~ Turnaround time is 5 weeks</li>
+          </ul>
+    </div>
+  )
+}
+
+export default Pricing
